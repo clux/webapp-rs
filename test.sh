51f138c7106fbb17f5e51aa1b5ee99f4de1d66da
clux <sszynrae@gmail.com>
2018-02-24 21:09:52 +0000

    use 0.0.0.0 over localhost everywhere

--- test.sh
+++ test.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 set -ex
 
-res=$(curl -s -X POST http://localhost:8000/posts -H "Content-Type: application/json" \
+res=$(curl -s -X POST http://0.0.0.0:8000/posts -H "Content-Type: application/json" \
   -d '{"title": "my life", "body": "memes"}')
 
 lastid="$(echo "${res}" | json id)"
 
 # can get it individually
-curl -s -X GET "http://localhost:8000/posts/${lastid}"
+curl -s -X GET "http://0.0.0.0:8000/posts/${lastid}"
 
 # post not published yet
-curl -s -X GET http://localhost:8000/posts | grep -v "${lastid}"
+curl -s -X GET http://0.0.0.0:8000/posts | grep -v "${lastid}"
 
 # publish
-curl -s -X PUT "http://localhost:8000/posts/${lastid}"
+curl -s -X PUT "http://0.0.0.0:8000/posts/${lastid}"
 
 # post now published
-curl -s -X GET http://localhost:8000/posts | grep "${lastid}"
+curl -s -X GET http://0.0.0.0:8000/posts | grep "${lastid}"
 
 # delete post
-curl -s -X DELETE "http://localhost:8000/posts/${lastid}"
+curl -s -X DELETE "http://0.0.0.0:8000/posts/${lastid}"
